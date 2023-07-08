3a0e0471c922487a7cbf3485ec79700985e98717
egork <egork@DESKTOP-NKPC04I>
2023-07-08 19:36:50 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dir = textBox1.Text;
+            Directory.CreateDirectory("data\\" + dir);
+
+            var sw = new StreamWriter("data\\" + dir + "data.ls");
+
+            string enctxt = Shifr.Encrypt(textBox1.Text);
+
+            sw.WriteLine(enctxt);
+
+            sw.Close();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string dir = textBox1.Text;
+
+            StreamReader sr = new StreamReader(Application.StartupPath + "\\data\\" + dir + "data.ls");
+            string line = sr.ReadLine();
+
+            textBox1.Text = Shifr.Rashifr(Convert.ToString(line));
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string dir = textBox2.Text;
+            Directory.CreateDirectory("data\\" + dir);
+
+            var sw = new StreamWriter("data\\" + dir + "data.ls");
+
+            string enctxt = Shifr.Encrypt(textBox1.Text);
+
+            sw.WriteLine(enctxt);
+
+            sw.Close();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string dir = textBox2.Text;
+
+            StreamReader sr = new StreamReader(Application.StartupPath + "\\data\\" + dir + "data.ls");
+            string line = sr.ReadLine();
+
+            textBox2.Text = Shifr.Rashifr(Convert.ToString(line));
+        }
+        
+    }
+}
---
+++ WindowsFormsApp3/shifr.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class Shifr
+    {
+        public static string IV = "asdesfhj.l/sdla;d;wl";
+        public static string Key = "fskal;fklask;dslf;asa";
+
+        public static string Encrypt(string decrypted)
+        {
+            byte[] textbytes = ASCIIEncoding.ASCII.GetBytes(decrypted);
+            AesCryptoServiceProvider endec = new AesCryptoServiceProvider();
+            endec.BlockSize = 128;
+            endec.KeySize = 256;
+            endec.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            endec.Key = ASCIIEncoding.ASCII.GetBytes(Key);
+            endec.Padding = PaddingMode.PKCS7;
+            endec.Mode = CipherMode.CBC;
+            ICryptoTransform icrypt = endec.CreateEncryptor(endec.Key, endec.IV);
+            byte[] enc = icrypt.TransformFinalBlock(textbytes, 0, textbytes.Length);
+            icrypt.Dispose();
+            return Convert.ToBase64String(enc);
+        }
+
+        public static string Rashifr(string encrypted)
+        {
+            byte[] textbytes = Convert.FromBase64String(encrypted);
+            AesCryptoServiceProvider endec = new AesCryptoServiceProvider();
+            endec.BlockSize = 128;
+            endec.KeySize = 256;
+            endec.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            endec.Key = ASCIIEncoding.ASCII.GetBytes(Key);
+            endec.Padding = PaddingMode.PKCS7;
+            endec.Mode = CipherMode.CBC;
+            ICryptoTransform icrypt = endec.CreateDecryptor(endec.Key, endec.IV);
+            byte[] enc = icrypt.TransformFinalBlock(textbytes, 0, textbytes.Length);
+            icrypt.Dispose();
+            return System.Text.ASCIIEncoding.ASCII.GetString(enc);
+        }
+    }
+}
